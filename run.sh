bc4af64f819a0a2636fc83fdf54a997ae4356dc5
sharad.singh <sharad.singh@flipkart.com>
2016-05-16 00:49:15 +0530

    making run.sh flexible

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 javac src/com/company/*.java
 cd src
 ##"/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  10 0
-java com.company.Main
+
+java com.company.Main "/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  $3 $4
