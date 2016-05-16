605da0ffa7667b486239a56b8033eb2bdc62aa11
sharad.singh <sharad.singh@flipkart.com>
2016-05-16 23:40:31 +0530

    changing number of thread and seeing the difference

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 javac src/com/company/*.java
 cd src
 ##"/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  10 0
-java com.company.Main "/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  20 1
+java com.company.Main "/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  4 0
