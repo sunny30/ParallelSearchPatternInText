ac65ee60615d3625c1f9e62d35cbc993fa154b20
sharad.singh <sharad.singh@flipkart.com>
2016-05-16 21:31:01 +0530

    adding one more run1.sh for manik's PC

---
+++ run1.sh
@@ -0,0 +1,4 @@
+javac src/com/company/*.java
+cd src
+##"/Users/sharad.singh/Devp/SearchPhraseInTest/D. H. Lawrence - Sons And Lovers.txt" "About this time"  10 0
+java com.company.Main "/home/ubuntu/haystack.txt" "Nate Malamud" 2 0
